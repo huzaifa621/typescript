de833da71c9538b97b488cb4bcbf66c63c6faecf
huzaifa621 <huzaifasheikh2021@gmail.com>
2023-06-29 15:27:48 +0530

    Add: resultGen in D1

--- D1/resultGen.js
+++ D1/resultGen.js
@@ -1,4 +1,49 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+var merge = function (leftArr, rightArr) {
+    var arr = [];
+    var i = 0;
+    var j = 0;
+    while (i < leftArr.length && j < rightArr.length) {
+        if (leftArr[i].avg > rightArr[j].avg) {
+            arr.push(leftArr[i]);
+            i++;
+        }
+        else {
+            arr.push(rightArr[j]);
+            j++;
+        }
+    }
+    arr.push.apply(arr, __spreadArray(__spreadArray([], leftArr.slice(i, leftArr.length), false), rightArr.slice(j, rightArr.length), false));
+    return arr;
+};
+var mergeSort = function (arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+    var mid = Math.floor(arr.length / 2);
+    var leftArr = arr.slice(0, mid);
+    var rightArr = arr.slice(mid, arr.length);
+    return merge(leftArr, rightArr);
+};
 var data = [
     { name: "Jack", science: 78, maths: 75, socialScience: 77 },
     { name: "Jimmy", science: 70, maths: 50, socialScience: 90 },
+    { name: "Cloe", science: 90, maths: 90, socialScience: 90 },
 ];
+var result = [];
+for (var _i = 0, data_1 = data; _i < data_1.length; _i++) {
+    var student = data_1[_i];
+    var avg = (student.maths + student.science + student.socialScience) / 3;
+    var obj = { name: student.name, avg: avg };
+    result.push(obj);
+}
+console.log(result);
+console.log(mergeSort(result));
--- D1/resultGen.ts
+++ D1/resultGen.ts
@@ -5,7 +5,55 @@ interface StudentData {
    socialScience: number;
 }
 
-let data: StudentData[] = [
+interface ResultData {
+   name: string;
+   avg: number;
+}
+
+const merge = (leftArr: ResultData[], rightArr: ResultData[]): ResultData[] => {
+   let arr: ResultData[] = [];
+   let i: number = 0;
+   let j: number = 0;
+   while (i < leftArr.length && j < rightArr.length) {
+      if (leftArr[i].avg > rightArr[j].avg) {
+         arr.push(leftArr[i]);
+         i++;
+      } else {
+         arr.push(rightArr[j]);
+         j++;
+      }
+   }
+   arr.push(
+      ...leftArr.slice(i, leftArr.length),
+      ...rightArr.slice(j, rightArr.length)
+   );
+   return arr;
+};
+
+const mergeSort = (arr: ResultData[]): ResultData[] => {
+   if (arr.length <= 1) {
+      return arr;
+   }
+   const mid: number = Math.floor(arr.length / 2);
+   const leftArr: ResultData[] = arr.slice(0, mid);
+   const rightArr: ResultData[] = arr.slice(mid, arr.length);
+   return merge(leftArr, rightArr);
+};
+
+const data: StudentData[] = [
    { name: "Jack", science: 78, maths: 75, socialScience: 77 },
    { name: "Jimmy", science: 70, maths: 50, socialScience: 90 },
+   { name: "Cloe", science: 90, maths: 90, socialScience: 90 },
 ];
+
+let result: ResultData[] = [];
+
+for (let student of data) {
+   const avg: number =
+      (student.maths + student.science + student.socialScience) / 3;
+   const obj: ResultData = { name: student.name, avg: avg };
+   result.push(obj);
+}
+
+console.log(result);
+console.log(mergeSort(result));
